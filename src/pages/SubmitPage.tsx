c2fcce30db805b2c627277d59d03dd62b0c37bf0
fatemeabedi <abedifateme1378@gmail.com>
2024-07-09 21:44:22 +0330

    edit submit page

--- src/pages/SubmitPage.tsx
+++ src/pages/SubmitPage.tsx
@@ -8,10 +8,14 @@ const SubmitPage: React.FC = () => {
     <div className="min-h-screen flex flex-col bg-gray-900">
       <Header />
       <main className="flex-1 p-8 border border-spacing-2">
+        <h1 className='text-white font-bold bg-center text-center text-4xl'>Submit Your Address </h1>
         <div className="container mx-auto ">         
             <SubmitDelivey/>
             <DeliveryMap/>
         </div>
+        <button className="bg-yellow-500 mt-12 ml-[10%] font-bold text-white py-2 px-4 rounded hover:bg-yellow-600 transition duration-300">
+            OK, send me delicious foods 
+            </button>
         
       </main>
       {/* <Footer /> */}
